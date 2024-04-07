76b82031638d2469d074e698988697fd49bb56c9
fewblocks <o.w.f.n.life@gmail.com>
2024-04-07 20:19:12 +0900

    first

---
+++ src/components/Results.tsx
@@ -0,0 +1,37 @@
+// Results.tsx
+
+type ResultsPropsType = {
+  results: {
+    country: string;
+    cityName: string;
+    temperature: string;
+    conditionText: string;
+    icon: string;
+  }
+
+}
+
+const Results =({results}:ResultsPropsType)=> {
+  const { country, cityName, temperature, conditionText, icon} = results;
+  return (
+    <>
+      {country && 
+        <div className='results-country'>{country}</div>
+      }
+      {cityName && 
+        <div className='results-city'>{cityName}</div>
+      }
+      {temperature && 
+      <div className='results-temp'>{temperature}°C</div>
+      }
+      {conditionText &&
+        <div className='results-condition'>
+          <img src={icon} alt="icon" />
+          <span>{conditionText}</span>
+        </div>
+      }
+    </>
+  )
+};
+
+export default Results;
